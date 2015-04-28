5c4365561f5202240d4502db6ea3a2a0e23aa48c
shirotsune <forgetgaming@gmail.com>
2015-04-28 23:29:53 +0300

    Source - Commit number: 2

--- Main.java
+++ Main.java
@@ -8,15 +8,26 @@ public class Main implements UserInterface
         *   Main tekee:
         *   1.  alustaa ohjelman ja luo graaffisen käyttöliittymän.
         *   2.  lukee CV:n sisällön ja karttatiedoston sisällön. (pelillistys!)
-        *   3.  Luodaan peli maailma ja CV tiedoston pohjalle.
-        
+        *   3.  Luodaan peli Map ja CV tiedoston pohjalta. (Konstruktoidaan Cardinal)
+        *   4.  Suoritetaan pelin suoritus looppi.
+    
         ::::: Luokat :::::
         * Main          
         - ohjelman ajo.
         
+        * Cardinal
+        - Pelin päärunko.
+        - Ylläpitää primääri looppia. (ns. main logiikka)
+    
         * GUI           
         - graaffinen käyttöliittymä
         
+        * CV
+        - lukee ja käsittelee CV:n sisällön ja muuttaa sen pelin sisäiseksi tarinaksi.
+        
+        * Map
+        - lukee ja käsittelee kartta tiedoston sisällön ja muuttaa sen pelin sisäiseksi tarinaksi.
+    
         * UserInterface
         - eventlistener
         */
@@ -51,7 +62,26 @@ public class Main implements UserInterface
         Map world = new Map(file_nameMap);
         CV story = new CV(file_nameCV);
     
+        /**********************************************************************
+         * 3. 
+         * Alustetaan pelimaailma CV ja Map filejen perusteella.
+         * Maailma generaattori on käytännössä vastuussa koko pelin rakenteesta.
+         * Pelin päälooppi kutsutaan myös tämän luokan kautta.  
+         **********************************************************************/
+        Cardinal CV_Art_Offline = new Cardinal(world, story);
+        /* Sword Art Online tribuutti. */
+        
+        /**********************************************************************
+         * 4.
+         * (Cardinalin API dokumentaatio löytyy Cardinal tiedoston sisältä.)
+         **********************************************************************/
     
+        
+        CV_Art_Offline.link_Start();
+        
+        /**************
+         * Main loppuu!
+         **************/
     }
     
 }
