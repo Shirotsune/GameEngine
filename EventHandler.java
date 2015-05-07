e8e936ae59cf7afd4439c3a1cb15c4099210668f
shirotsune <forgetgaming@gmail.com>
2015-05-07 15:33:28 +0300

    Source - Commit number: 5 (After) - Skeleton Code-clean-up

--- EventHandler.java
+++ EventHandler.java
@@ -8,32 +8,13 @@ public class EventHandler implements KeyListener
 {
 
     private static Player player;
-    private static boolean isAction; //Skeleton Code
-    private static boolean actionD; //Skeleton Code
-    private static boolean actionA; //Skeleton Code
-    private static boolean actionW; //Skeleton Code
     /* Utilises Cardinal version. Player cordinates are not relevant for action listener. */
 
     public EventHandler(Player player) // -!!!- Constructor functions well -!!!-
     {
         this.player = player;
-
-        this.actionA = false; //Skeleton Code
-        this.actionD = false; //Skeleton Code
-        this.actionW = false; //Skeleton Code
     }
-
-    boolean getPlayerState() /* Skeleton Code */
-    {
-
-        if (actionD | actionA | actionW == true)
-        {
-            return true;
-        }
-
-        return false;
-    } /* Skeleton Code */
-
+ 
     @Override
     public void keyPressed(KeyEvent e)
     {
@@ -42,17 +23,14 @@ public class EventHandler implements KeyListener
         if (e.getKeyCode() == (KeyEvent.VK_D))
         {
             player.right();
-            actionD = true; //Skeleton Code
         }
         if (e.getKeyCode() == (KeyEvent.VK_A))
         {
             player.left();
-            actionA = true; //Skeleton Code
         }
         if (e.getKeyCode() == (KeyEvent.VK_W))
         {
             player.jump();
-            actionW = true; //Skeleton Code
         }
 
     }
@@ -63,17 +41,14 @@ public class EventHandler implements KeyListener
         if (e.getKeyCode() == (KeyEvent.VK_D))
         {
             player.rightEnds();
-            actionD = false; //Skeleton Code
         }
         if (e.getKeyCode() == (KeyEvent.VK_A))
         {
             player.leftEnds();
-            actionA = false; //Skeleton Code
         }
         if (e.getKeyCode() == (KeyEvent.VK_W))
         {
             player.jumpEnds();
-            actionW = false; //Skeleton Code
         }
 
 
