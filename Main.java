a470b760f7b2fa33bda78e4ddbfef18d61246822
Tiamo Laitakari <forgetgaming@gmail.com>
2015-05-12 17:08:06 +0300

    GameEngine Main
    Documentation and commentary of the main class.

--- Main.java
+++ Main.java
@@ -6,92 +6,38 @@ public class Main
     /* 
      * Copyright: Tiamo Laitakari (Tiamo.Laitakari(ät)cs.helsinki piste äfii
     
-     *   Main tekee:
-     *   1.  alustaa ohjelman ja luo graaffisen käyttöliittymän.
-     *   2.  lukee CV:n sisällön ja karttatiedoston sisällön. (pelillistys!)
-     *   3.  Luodaan peli Map ja CV tiedoston pohjalta. (Konstruktoidaan Cardinal)
-     *   4.  Suoritetaan pelin suoritus looppi.
+     * Real-time game engine for side-scroller game.
+     * This is a stripped down example of how to do a game engine that reacts to player input in a FPS enviorement.
+     * 
+     * Classes::
+     *
+     * Cardinal         - Maingame loop
+     * Player           - Player constructor
+     * GraphicsEngine   - Rendering
+     * GUI              - Rendering
+     * EventHandler     - Handles keybord input.
     
-     ::::: Luokat :::::
-     * Main          
-     - ohjelman ajo.
-        
-     * Cardinal
-     - Pelin päärunko.
-     - Ylläpitää primääri looppia. (ns. main logiikka)
-    
-     * GUI           
-     - graaffinen käyttöliittymä
-        
-     * CV
-     - lukee ja käsittelee CV:n sisällön ja muuttaa sen pelin sisäiseksi tarinaksi.
-        
-     * Map
-     - lukee ja käsittelee kartta tiedoston sisällön ja muuttaa sen pelin sisäiseksi tarinaksi.
-        
-     * GraphicsEngine
-     - Piirtää kaikki pelin objektit.
-    
-     * Player
-     - pelaaja hahmo.
-           
-     * EventHandler
-     - käsittelee pelaajan liikkeet.
      */
 
     public static void main(String[] args)
     {
 
-        /**
-         * *******************************************************************
-         * 1. x-axis resoluutio y-axis resoluutio alustaa ja ajaa
-         * käyttöliittymän
-         ********************************************************************
-         */
+        /****************************************************************
+         * Starting resolution.
+         ****************************************************************/
         int resolutionX = 800;
         int resolutionY = 640;
 
-        GUI start = new GUI(resolutionX, resolutionY);
+        GUI start = new GUI(resolutionX, resolutionY); //Start up rendering a box.
 
 
 
-        /**
-         * *******************************************************************
-         * 2. file_nameCV - CV:n sijainti file_nameMap - karttatiedosto lukee
-         * muistiin tarpeelliset eri luokkien rakentamiseen tarvittavat tiedot.
-         * Map ja CV olio luodaan näiden kahden perusteella. Huom! Map ja CV
-         * olio halutaan vain konstruktoida kertaalleen.
-         ********************************************************************
-         */
-        String file_nameCV = "";
-        String file_nameMap = "";
-
-        Map world = new Map(file_nameMap);
-        CV story = new CV(file_nameCV);
-
-        /**
-         * ********************************************************************
-         * 3. Alustetaan pelimaailma CV ja Map filejen perusteella. Maailma
-         * generaattori on käytännössä vastuussa koko pelin rakenteesta. Pelin
-         * päälooppi kutsutaan myös tämän luokan kautta.  
-         *********************************************************************
-         */
-        Cardinal CV_Art_Offline = new Cardinal(world, story, start);
+        Cardinal CV_Art_Offline = new Cardinal(start); //Initialize the main game loop with it's constructor.
 
 
-        /**
-         * ********************************************************************
-         * 4. (Cardinalin API dokumentaatio löytyy Cardinal tiedoston sisältä.)
-         *********************************************************************
-         */
+        CV_Art_Offline.link_Start(); //Main game loop.
 
-        CV_Art_Offline.link_Start();
-
-        /**
-         * ************
-         * Main loppuu!
-         *************
-         */
+// End of main.
     }
 
 }
