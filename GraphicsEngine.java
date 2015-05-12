43fc3bd82886da1ab9f9af1b0cba9116d4c9d214
Tiamo Laitakari <forgetgaming@gmail.com>
2015-05-12 17:18:00 +0300

    Mesh for rendering objects in the game. (Currently only player)
    Mesh for rendering stuff in game. Currently only renders player character.

--- GraphicsEngine.java
+++ GraphicsEngine.java
@@ -4,15 +4,11 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 import java.awt.*;
 
-/* This class will be called for several times. Will handle different images and etc.*/
 
 public class GraphicsEngine extends JPanel {
 
     private Player player;
-    /*
-    public GraphicsEngine() {
-        super.setBackground(Color.GRAY);
-    }*/
+
     
     public GraphicsEngine(Player player) {
         this.player = player;
